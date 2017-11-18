9f566bbe92a19daaa297ad5e6a92aedb02d60ec8
yunfzhai <yunfzhai@outlook.com>
2017-11-19 00:31:23 +0800

    no common

--- src/example.py
+++ src/example.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, make_response, jsonify,g,url_for
+from flask import Flask, render_template, make_response, jsonify,g,url_for,redirect,request,flash
 from functools import wraps
 from flask_cache import Cache
 from qpython import qconnection as qc
 import numpy as np
+import json
 app=Flask(__name__)
 
 # app configuration
@@ -21,6 +22,12 @@ def get_db():
         g.q_db = connect_db()
     return g.q_db
 
+def fac_trace_table(range_a,checkdate_a,from_a,end_a,type_a):
+    '''range 范围，checkdate查看日期 from开始日期 end结束日期 
+    type指定多空组合，还是一定数目的多头 f4table f4tablefg f4tablefgg三种类型'''
+    qcmd = type_a+'[`'+range_a+';'+checkdate_a+';'+from_a+';'+end_a+']'
+    return qcmd
+
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
@@ -38,13 +45,37 @@ def close_db(error):
 #         rst.headers['Access-Control-Allow-Headers'] = allow_headers
 #         return rst
 #     return wrapper_fun
+@app.route('/')
+def index():
+    return redirect(url_for('datatest'))
+
+@app.route('/update_table', methods=['POST'])
+def update_table():
+    # password = request.form.get('password')
+    # username = request.args.get('username')
+    type_a = request.form.get('type')
+    rng = request.form.get('rng')
+    cd_a = request.form.get('checkdate')
+    fm_a = request.form.get('fmdate')
+    ed_a = request.form.get('eddate')
+    qcmd = fac_trace_table(rng,cd_a,fm_a,ed_a,type_a)
+    flash(qcmd)
+    q=get_db()
+    t1=q.sync(qcmd,pandas=True)
+    t1.fac=[str(x,encoding='utf-8') for x in t1.fac]
+    t1.iloc[:,1:]=np.around(t1.iloc[:,1:],3)    
+    t11 = [list(g) for g in t1.values]  #横着用
+    data= { 'test':t11,'thefuck':'刚刚执行命令:'+qcmd}
+    return jsonify(data)
+
+
 
 @app.route('/wtf/wtf/dtest/wtf')
 # @allow_cross_domain
 # @cache.cached(timeout=2, key_prefix='random')
 def datatest():
     q=get_db()
-    t1=q.sync('f4table[`.zall;2017.12.31;2017.01.01;2017.12.31]',pandas=True)
+    t1=q.sync('2#f4table[`.zall;2017.12.31;2017.01.01;2017.12.31]',pandas=True)
     t1.fac=[str(x,encoding='utf-8') for x in t1.fac]
     t1.iloc[:,1:]=np.around(t1.iloc[:,1:],3)    
     t11 = [list(g) for g in t1.values]  #横着用
@@ -61,6 +92,10 @@ def datatest():
 @app.route('/test/')
 def test():
     return render_template('test.html',names=['因子名称','最近5日','d10','d20','d60','d120','d240','dfu'])
+
+@app.route('/test2/')
+def test2():
+    return render_template('test_backup.html',names=['因子名称','最近5日','d10','d20','d60','d120','d240','dfu'])    
 
 if __name__ == '__main__':
     app.run(debug=True)
